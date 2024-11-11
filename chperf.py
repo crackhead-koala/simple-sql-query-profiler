9795485f393cdb761247277ac1cf465f1a1291b4
Sergey Suvorov <suvorov@findmykids.org>
2024-11-11 20:57:41 +0100

    add support for comparing multiple queries

--- chperf.py
+++ chperf.py
@@ -1,9 +1,9 @@
 import os
-import dotenv
 import argparse
 import time
 import statistics
 
+import dotenv
 import tqdm
 import clickhouse_driver as cd
 
@@ -17,7 +17,11 @@ if __name__ == '__main__':
             'a database connection.'
     )
 
-    parser.add_argument('query', help='path to the file containing a single SQL query to measure')
+    parser.add_argument(
+        'queries',
+        nargs='+',
+        help='path to the file containing a single SQL query to measure'
+    )
     parser.add_argument(
         '-t', '--times',
         type=int,
@@ -45,40 +49,59 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    with open(args.query, 'r') as q:
-        query = q.read()
-
     if args.use_env:
         dotenv.load_dotenv()
 
-    query_execution_times = []
-    _range = tqdm.trange(args.times, ncols=100) \
-        if args.verbose \
-        else range(args.times)
-    for _ in _range:
-        client = cd.Client(
-            host=args.host or os.getenv('HOST'),
-            port=args.port or int(os.getenv('PORT')),
-            database=args.database or os.getenv('DATABASE'),
-            user=args.username or os.getenv('USERNAME'),
-            password=args.password or os.getenv('PASSWORD'),
-            secure=False,
-            verify=False
+    queries: dict[str, str] = {}
+    for query_file in args.queries:
+        with open(query_file, 'r') as q:
+            queries[query_file] = q.read()
+
+    query_execution_times: dict[str, dict[str, float]] = {}
+    for query_file, query in queries.items():
+        _range = tqdm.trange(args.times, ncols=100) \
+            if args.verbose \
+            else range(args.times)
+
+        print(f'measuring {query_file}')
+
+        iter_execution_times: list[float] = []
+        for _ in _range:
+            client = cd.Client(
+                host=args.host or os.getenv('HOST'),
+                port=args.port or int(os.getenv('PORT')),
+                database=args.database or os.getenv('DATABASE'),
+                user=args.username or os.getenv('USERNAME'),
+                password=args.password or os.getenv('PASSWORD'),
+                secure=False,
+                verify=False
+            )
+
+            with client:
+                start = time.perf_counter()
+                _ = client.execute(query)
+                end = time.perf_counter()
+                iter_execution_times.append(end - start)
+
+        time_min = min(iter_execution_times)
+        time_max = max(iter_execution_times)
+        time_avg = statistics.mean(iter_execution_times)
+        time_std = statistics.stdev(iter_execution_times)
+
+        query_execution_times[query_file] = {
+            'min': time_min,
+            'max': time_max,
+            'avg': time_avg,
+            'std': time_std
+        }
+
+    print(f'\nresults over {args.times} iterations:')
+
+    max_file_name_len = max(map(len, query_execution_times.keys()))
+    for query_file, results in query_execution_times.items():
+        print(
+            f'{query_file: >{max_file_name_len}}: '
+            f'min: {time_min:.2f}, '
+            f'max: {time_max:.2f}, '
+            f'avg (±std): {time_avg:.2f}s. ±{time_std:.2f}s.'
         )
-
-        with client:
-            start = time.perf_counter()
-            _ = client.execute(query)
-            end = time.perf_counter()
-            query_execution_times.append(end - start)
-
-    time_min = min(query_execution_times)
-    time_max = max(query_execution_times)
-    time_avg = statistics.mean(query_execution_times)
-    time_std = statistics.stdev(query_execution_times)
-
-    print(
-        f'min: {time_min:.2f}, '
-        f'max: {time_max:.2f}, '
-        f'avg (±std): {time_avg:.2f}s. ±{time_std:.2f}s. ({args.times} iterations)'
-    )
